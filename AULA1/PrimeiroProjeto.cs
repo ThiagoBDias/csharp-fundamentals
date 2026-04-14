3d5cf276e5bfe70867b377622dbf48bfcac6282c
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-14 11:27:49 -0300

    Update PrimeiroProjeto.cs

--- AULA1/PrimeiroProjeto.cs
+++ AULA1/PrimeiroProjeto.cs
@@ -8,6 +8,14 @@ namespace PrimeiroProjeto
         static void Main(string[] args)
         {
             Console.WriteLine("Olá, Mundo!");
+            int idade = 25;
+            Console.WriteLine("Minha idade é: " + idade);
+            double altura = 1.75;
+            Console.WriteLine("Minha altura é: " + altura);
+            char genero = 'M';
+            Console.WriteLine("Meu gênero é: " + genero);
+            bool estudante = true;
+            Console.WriteLine("Sou estudante: " + estudante);
         }
     }
 }
