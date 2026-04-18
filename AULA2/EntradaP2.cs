445967887ecc8f6bfa624dbc96cccf9d110e094d
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-18 12:19:25 -0300

    Update EntradaP2.cs

--- AULA2/EntradaP2.cs
+++ AULA2/EntradaP2.cs
@@ -7,6 +7,10 @@ namespace AULA2
         static void Main(string[] args)
         {
             int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: " + n1);
+            Console.WriteLine("Você digitou: " + ch);
 
             
         }
