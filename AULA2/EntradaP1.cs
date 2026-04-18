f3c4d90bcb2f76a2285cc570213a18e0919d54e4
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-18 09:48:49 -0300

    Update EntradaP1.cs

--- AULA2/EntradaP1.cs
+++ AULA2/EntradaP1.cs
@@ -13,7 +13,17 @@ namespace AULA2
             Console.WriteLine("A frase digitada foi: " + frase);
              Console.WriteLine("A frase digitada foi: " + x);
              Console.WriteLine("A frase digitada foi: " + y);
-        
+
+             string[] palavras = frase.Split(' ');
+            Console.WriteLine("As palavras digitadas foram:");  
+            string [] palavras1 = palavras[0];
+            string [] palavras2 = palavras[1];
+            string [] palavras3 = palavras[2];  
+
+                Console.WriteLine(palavras1);
+                Console.WriteLine(palavras2);
+                Console.WriteLine(palavras3);
+
         }
     }
 }
