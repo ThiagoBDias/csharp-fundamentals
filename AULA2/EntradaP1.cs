cb693ad8177ebfaeee179ea01b3df1872ea5e6fd
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-18 09:41:24 -0300

    Create EntradaP1.cs
    algumas entradas

---
+++ AULA2/EntradaP1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace AULA2
+{
+    class EntradaP1
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Digite uma frase e palavras: ");
+            string frase = Console.ReadLine();
+             string  x = Console.ReadLine();    
+             string  y = Console.ReadLine();
+            Console.WriteLine("A frase digitada foi: " + frase);
+             Console.WriteLine("A frase digitada foi: " + x);
+             Console.WriteLine("A frase digitada foi: " + y);
+        
+        }
+    }
+}
