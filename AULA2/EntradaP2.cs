ec60759431a98f87c269f132acbd3af99bfdf7dd
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-18 12:25:03 -0300

    Update EntradaP2.cs

--- AULA2/EntradaP2.cs
+++ AULA2/EntradaP2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace AULA2
 {
@@ -8,10 +9,22 @@ namespace AULA2
         {
             int n1 = int.Parse(Console.ReadLine());
             char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            string[] vet = Console.ReadLine().Split(' ');
+                string nome = vet[0];
+                char sexo = char.Parse(vet[1]);
+                int idade = int.Parse(vet[2]);
+                double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+    
+                Console.WriteLine("Você digitou: " + nome);
+                Console.WriteLine("Você digitou: " + sexo);
+                Console.WriteLine("Você digitou: " + idade);
+                Console.WriteLine("Você digitou: " + altura.ToString("F2", CultureInfo.InvariantCulture));
+            
             Console.WriteLine("Você digitou: " + n1);
             Console.WriteLine("Você digitou: " + ch);
-
+            Console.WriteLine("Você digitou: " + n2.ToString("F2", CultureInfo.InvariantCulture));
             
         }
     }
