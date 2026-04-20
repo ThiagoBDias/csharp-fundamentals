cb9c7588215bcf66fe8bd33ea1eaaf662272ce01
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-20 11:36:02 -0300

    Update Matematica.cs

--- AULA2/Matematica.cs
+++ AULA2/Matematica.cs
@@ -9,29 +9,6 @@ public class Matematica
     double a,b,c;
 
     a = Math.sqrt(x);
-
-
-    public static int Soma(int a, int b)
-    {
-        return a + b;
-    }
-
-    public static int Subtracao(int a, int b)
-    {
-        return a - b;
-    }
-
-    public static int Multiplicacao(int a, int b)
-    {
-        return a * b;
-    }
-
-    public static double Divisao(int a, int b)
-    {
-        if (b == 0)
-        {
-            throw new DivideByZeroException("Divisão por zero não é permitida.");
-        }
-        return (double)a / b;
-    }
+    b = Math.sqrt(y);
+    c = Math.sqrt(25.0);
 }
