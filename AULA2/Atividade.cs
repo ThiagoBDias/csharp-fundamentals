553898c330bafa6c593a1086308aadb4faf608bc
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-20 10:38:36 -0300

    atividade

--- AULA2/Atividade.cs
+++ AULA2/Atividade.cs
@@ -8,13 +8,18 @@ namespace AULA2
         {
             Console.WriteLine("entre com seu nome completo:");
             string nome = Console.ReadLine();
-            Console.WriteLine("Você digitou: " + nome);
             Console.WriteLine("Quantos quartos tem na sua casa?");
             string quartos = Console.ReadLine();
             Console.WriteLine("Você digitou: " + quartos); 
             Console.WriteLine("Entre com o preço de um produto:");
             string preco = Console.ReadLine(); 
             Console.WriteLine("Você digitou: " + preco);
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] dados = Console.ReadLine().Split(' ');
+            string ultimoNome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2]);
+            Console.WriteLine("Você digitou: " + ultimoNome + ", " + idade + " anos, " + altura + "m");
         }
     }
 }
