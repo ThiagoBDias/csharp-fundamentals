6abea977f05e6625fa0760add722d315f6725c19
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2026-04-30 11:19:40 -0300

    atualização
    desafio

--- beecrowd/BuscaInternet.cs
+++ beecrowd/BuscaInternet.cs
@@ -4,18 +4,8 @@ class URI {
     static void Main(string[] args) { 
         int t = int.Parse(Console.ReadLine());
         
-       int primeiroLink = 0;
-        for (int i = 0; i < t; i++) {
-            string[] links = Console.ReadLine().Split(' ');
-            int link1 = int.Parse(links[0]);
-            int link2 = int.Parse(links[1]);
-
-            if (link1 < link2) {
-                primeiroLink = link1;
-            } else {
-                primeiroLink = link2;
-            }
-        }
+        int segundoLink = t * 2;
+        int primeiroLink = segundoLink * 2;
 
         Console.WriteLine(primeiroLink);
     }
